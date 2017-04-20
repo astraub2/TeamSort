15c121eef5843938bd9df57b759feec43f2cc3e7
astraub2 <astraub2@uoregon.edu>
2017-04-20 11:26:22 -0700

    fixed python indent problem

--- src/TeamSort.py
+++ src/TeamSort.py
@@ -2,32 +2,32 @@ import psycopg2
 #psycopg2 is our database connector
 
 def main():
-	#the var in conn will change, this is temp
-	conn = psycopg2.connect(dbname='teamsort')
-    cur = conn.cursor()
+        #the var in conn will change, this is temp
+        conn = psycopg2.connect(dbname='teamsort')
+        cur = conn.cursor()
 
-    # #Below is a general query format for database info
-    # cur.execute('SELECT username FROM users;')
-    
-    # #essentially check if the query returned anything
-    # try:
-    #     result = cur.fetchone()
-    # except ProgrammingError:
-    #     result = None
-    # if result == None:
+        # #Below is a general query format for database info
+        cur.execute('SELECT username FROM users;')
 
-    # else:
-    # 	#result may hold a Boolean, int, string, etc,
-    # 	#check if result is what you want
+        # #essentially check if the query returned anything
+        # try:
+        #     result = cur.fetchone()
+        # except ProgrammingError:
+        #     result = None
+        # if result == None:
 
-    # #following is a basic insert into DB query
+        # else:
+        #   #result may hold a Boolean, int, string, etc,
+        #   #check if result is what you want
 
-    # #cur.execute('INSERT INTO roles (role_name) VALUES(%s);',(role,))
+        # #following is a basic insert into DB query
+
+        # #cur.execute('INSERT INTO roles (role_name) VALUES(%s);',(role,))
          
-    # #important, anytime the database is updated, like
-    # #a team is made, we must run following command:
-    # conn.commit()
+        # #important, anytime the database is updated, like
+        # #a team is made, we must run following command:
+        # conn.commit()
 
 
 if __name__ == "__main__":
-	main()
+main()
