29c317a2a98f2ce58be52f1b3fb08a3bb00e68d5
Anisha Aggarwal <anishaa@Anishas-MacBook-Air-2.local>
2017-04-24 17:41:42 -0700

    new features

--- sampleApp.py
+++ sampleApp.py
@@ -1,26 +1,62 @@
 #sample app
 
-import tkinter as tk
+from Tkinter import *
+import webbrowser
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
+class Application(Frame):
 
     def say_hi(self):
-        print("hi there, everyone!")
+        print "hi there, everyone!"
+    def callback(self):
+        webbrowser.open_new(r"http://www.google.com")
 
-root = tk.Tk()
+    def userinput(self):
+        a = raw_input(self.E1.get())
+        print a
+
+    #def getSize(self):
+        
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "right"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side": "left"})
+
+        self.LINK = Button(self)
+        self.LINK["text"] = "Google Hyperlink"
+        self.LINK["command"] = self.callback
+        self.LINK.pack({"side": "left"})
+
+        # self.label1 = Label( root, text="Group Size:")
+        # self.E1 = Entry(root, bd =5)
+        # self.label1.pack({"side": "bottom"})
+        # self.E1.pack({"side": "bottom"})
+        self.label1 = Label( root, text="Group Size:")
+        self.E1 = Entry(root, bd =5)
+        self.label1.pack({"side": "left"})
+        self.E1.pack({"side": "left"})
+
+        self.submit = Button(self)
+        self.submit["text"] = "Submit"
+        self.submit["command"] = self.userinput
+        self.submit.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+
+root = Tk()
 app = Application(master=root)
 app.mainloop()
+root.destroy()
